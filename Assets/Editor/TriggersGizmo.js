da45c6cda4e9fdb2b9e6986de938e2ff059b2fdd
Caleb Jones <bionicle.dragon@gmail.com>
2013-04-02 15:12:09 -0400

    Added gizmos for sphere triggers Added menu items for adding triggers Added prefabs for sphere triggers and box triggers

--- Assets/Editor/TriggersGizmo.js
+++ Assets/Editor/TriggersGizmo.js
@@ -2,7 +2,26 @@
 
 @DrawGizmo (GizmoType.NotSelected | GizmoType.Pickable)
 static function DrawTriggerArea(trigger : TriggerArea, gizmoType : GizmoType) {
-	Gizmos.matrix = trigger.transform.localToWorldMatrix;
 	Gizmos.color = trigger.color;
-	Gizmos.DrawCube (Vector3.zero, Vector3.one);
+	if (trigger.collider.GetType() == BoxCollider) {
+		Gizmos.matrix = trigger.transform.localToWorldMatrix;
+		Gizmos.DrawCube (Vector3.zero, Vector3.one);
+	}
+	if (trigger.collider.GetType() == SphereCollider) {
+		var radius : float = trigger.GetComponent.<SphereCollider>().radius * 
+				Mathf.Max(trigger.transform.localScale.x, trigger.transform.localScale.y,
+				trigger.transform.localScale.z);
+		
+		Gizmos.DrawSphere(trigger.transform.position, radius);
+	}
+}
+
+@MenuItem ("GameObject/Create Trigger/Box")
+static function CreateBoxTrigger () {
+	
+}
+
+@MenuItem ("GameObject/Create Trigger/Sphere")
+static function CreateSphereTrigger () {
+	
 }
